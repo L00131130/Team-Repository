de821e6b9eb97899a7ecffbb90bbd3760c49dfca
L00131130 <l00131130@student.lyit.ie>
2021-01-18 19:58:49 +0000

    Update FindCustomer.java
    removed code from end of class as per meeting will edit until ready for main branch next week

--- Airlines Reservation/src/java/main/FindCustomer.java
+++ Airlines Reservation/src/java/main/FindCustomer.java
@@ -30,32 +30,7 @@ public class FindCustomer implements Serializable
         return(this.customerid);
     }
     
-    public Vector getResult()
-    {
-        Vector v = new Vector();
-        try
-        {
-            Class.forName("sun.jdbc.odbc.JdbcOdbcDriver");
-            Connection con = DriverManager.getConnection("jdbc:odbc:CustomerDataSource","","");
-            PreparedStatement stat = con.prepareStatement("SELECT * from customer where cust_id= ? ");
-            stat.setString(1,customerid);
-            ResultSet rs = stat.executeQuery();
-            if(rs.next())
-            {
-                v.addElement(rs.getString("cust_name"));
-                v.addElement(rs.getString("address"));
-                v.addElement(rs.getString("city"));
-                
-            }
-            
-        }
-        catch(Exception ex)
-        {
-            ex.printStackTrace();
-        }
-        
-        this.result = v;
-        return v;
+   
       }
     
     }
